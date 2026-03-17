dc5fae453aad39fb38120003aab72b0ec5ac2409
sefas2 <sefkanaskin@gmail.com>
2026-03-17 20:48:06 +0000

    Add timestamp to health check log

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import requests
 import os
+import datetime
+
 
 URL = os.environ.get('TARGET_HOST')
 response = requests.get(URL)
-status = f"Health check successful:{response.status_code}"
+time_date = datetime.datetime.now()
+status = f"{time_date} - Health check successful:{response.status_code}"
+print(status)
 
 with open('/app/report.txt', 'w') as f:
     f.write(status)
